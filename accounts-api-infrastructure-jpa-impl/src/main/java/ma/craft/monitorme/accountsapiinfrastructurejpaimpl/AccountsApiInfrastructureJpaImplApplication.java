f5498c964fdd6310137aec7485d6b6069c9ef691
Yassine SABEQ <sabeq.yassine15@gmail.com>
2018-09-03 17:31:26 +0100

    [Clean Architecture software design] Create layers application, domain, infrastructure

---
+++ accounts-api-infrastructure-jpa-impl/src/main/java/ma/craft/monitorme/accountsapiinfrastructurejpaimpl/AccountsApiInfrastructureJpaImplApplication.java
@@ -0,0 +1,12 @@
+package ma.craft.monitorme.accountsapiinfrastructurejpaimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApiInfrastructureJpaImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApiInfrastructureJpaImplApplication.class, args);
+	}
+}
