8453d6748a2dac23cde7452cb6770af6360eaa02
bahhou <nouredine.bahou@gmail.com>
2018-09-05 16:05:56 +0100

    [add project] added project test

---
+++ accounts-api-impl/src/main/java/ma/craft/monitorme/accountsapiimpl/AccountsApiImplApplication.java
@@ -0,0 +1,12 @@
+package ma.craft.monitorme.accountsapiimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApiImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApiImplApplication.class, args);
+	}
+}
