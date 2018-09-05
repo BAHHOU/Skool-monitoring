1349119387a4b40f9575f786b6b77e2b129b3ff6
bahhou <nouredine.bahou@gmail.com>
2018-09-05 11:51:42 +0100

    [sonar config] edited build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${T
       echo 'Checking Quality Gates'
       mvn -B clean verify sonar:sonar \
       -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.login=173a6f3663105a109acf1c78c43bf623de5a5377
+      -Dsonar.login=e18458dd7267847646de994362e0d612569ae279
       -Dsonar.projectKey=BAHHOU_MonitorMe \
       -Dsonar.organization=bahhou-github \
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Build and analyze pull request'
       mvn -B clean verify sonar:sonar \
       -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.login=173a6f3663105a109acf1c78c43bf623de5a5377
+      -Dsonar.login=e18458dd7267847646de994362e0d612569ae279
       -Dsonar.projectKey=BAHHOU_MonitorMe \
       -Dsonar.organization=bahhou-github \
       -Dsonar.github.oauth=d9609c126cb444e6b8b512feee80be1d9602fcdb \
